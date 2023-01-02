ca1e1782b32c69b4d1171b75d50d0670af0b99d7
ROWLEYES\donat <bahattin.ercan.43@gmail.com>
2023-01-02 23:42:21 +0300

    fps controller

---
+++ Assets/Eray/Scripts/Grab.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    public bool canGrab = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("I can grab");
+            canGrab = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("I can not grab");
+            canGrab = false;
+        }
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * MouseX);
+    }
+}
